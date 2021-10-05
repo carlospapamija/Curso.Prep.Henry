ec002757406571ecccde014fb5f4b83f7439b388
carlospapamija <papamija.car@gmail.com>
2021-10-05 17:06:36 -0500

    avance

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -38,6 +38,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array[array.length] = elemento;
+  return array
 }
 
 
@@ -46,6 +48,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento); 
+  return array;
 }
 
 
@@ -55,6 +59,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(' ');
 }
 
 
@@ -62,6 +67,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for(var i = 0; i < array.length; i++){
+    if(array[i] === elemento) {
+      return true
+    }
+  }
+   return false
 }
 
 
@@ -69,7 +80,13 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
+
 
 
 function promedioResultadosTest(resultadosTest) {
